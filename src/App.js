54ddc5963acdc247d25530fdf716a4cffecefb58
lior atar <lioratar@h-MacBook-Pro-sl-lior-2.local>
2022-01-29 23:05:20 +0200

    Added docs and improved the UI

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import Table from './components/Table';
 import BarChart from './components/BarChart';
 import { ROOT } from './Consts';
 
-// Adding docs(documentation string) to everything
-// Get the vehicle with the highest sum of population
-// UI
-// Caching - Axioa multiple times same request
-// Var / Let ? What is better?
 function App() {
 
   const [parsedVehicles, setParsedVehicles] = useState([]);
@@ -19,37 +14,54 @@ function App() {
   const [isFinished, setIsFinished] = useState(false);
 
   useEffect(() => {
+    /** Gets all vehicles if the currewnt list is empty */
     if (parsedVehicles.length === 0) {
       GetVehicles();
     }
 
+    /** Gets the vehicles with the highest sum of population */
     if (isFinished) {
       GetVehicleWithHighestSum();
     }
   }, [isFinished])
 
   /**
-     * Get all the vehicles
-     */
+   * Get all the vehicles and update the parsed vehicles state
+   */
   const GetVehicles = async () => {
+    /** Gets vehicles at current page */
     var resVehicles = await axios.get(`${ROOT}vehicles`);
 
+    /** Gets each vehicles pilots and planets and update the parsedVehicles state */
     await ParseVehicles(resVehicles.data.results);
 
+    /** Check if there is a next page to vehicles */
     while (resVehicles.data.next !== null) {
+      /** Gets vehicles at current page */
       resVehicles = await axios.get(resVehicles.data.next);
+      /** Gets each vehicles pilots and planets and update the parsedVehicles state */
       await ParseVehicles(resVehicles.data.results);
     }
 
+    /** Update that all requested vehicles was fetched */
     setIsFinished(true);
   }
 
+  /**
+   * Gets each vehicle pilots and plantes, then update the parsed vehicles state
+   * @param {*} vehicles Current vehicles that was fetched
+   */
   const ParseVehicles = async (vehicles) => {
+    /** Go thorugh all the vehicles */
     for (const vehicle of vehicles) {
+
+      /** Check if the current vehicle has pilots */
       if (vehicle.pilots.length > 0) {
         var pilotsUrl = vehicle.pilots;
+        /** Fetch the vehicle pilots and their planet */
         const [pilots, planets] = await ParsePilots(pilotsUrl);
 
+        /** Check if pilots is not empty */
         if (pilots.length > 0) {
           var parsedVehicle = {
             "name": vehicle.name,
@@ -58,27 +70,39 @@ function App() {
             "totalPopulation": SumPopulation(planets)
           };
 
+          /** Update the parsed vehicles state with the new value */
           setParsedVehicles(prevValue => [...prevValue, parsedVehicle]);
         }
       }
     }
   }
 
+  /**
+   * Fetch pilots and their planets
+   * @param {*} pilotsUrl Pilots url to fetch each pilot data
+   * @returns pilots and their plantes
+   */
   const ParsePilots = async (pilotsUrl) => {
     var pilots = [];
     var planets = {};
 
-    // TODO: check foreach
+    /** Go thorugh all the pilots url */
     for (const pilotUrl of pilotsUrl) {
+      /** Fetch the pilot data */
       var resPilot = await axios.get(pilotUrl);
+
+      /** Fetch the planet data */
       var resPlanet = await axios.get(resPilot.data.homeworld);
 
       var pilotName = resPilot.data.name;
       var planetName = resPlanet.data.name;
       var population = resPlanet.data.population
 
+      /** Check if the population of the current planet is not unknown */
       if (population !== "unknown") {
+        /** Add the current pilot name to the pilots list */
         pilots.push(pilotName);
+        /** Add the current planet name and population to the planets object */
         planets[planetName] = population;
       }
     }
@@ -86,9 +110,15 @@ function App() {
     return [pilots, planets];
   }
 
+  /**
+   * Sum all planets population
+   * @param {*} planets The planets to sum their population
+   * @returns Sum of population
+   */
   const SumPopulation = (planets) => {
     var sum = 0;
 
+    /** Go thorugh each planet population and its value to sum */
     for (const population of Object.values(planets)) {
       sum += parseInt(population);
     }
@@ -96,17 +126,26 @@ function App() {
     return sum;
   }
 
+  /**
+   * Gets and update the vehicles with the highest population
+   */
   const GetVehicleWithHighestSum = () => {
     var highestSum = 0;
 
+    /** Go thorugh each vehicle from the parsed vehicles state */
     for (const vehicle of parsedVehicles) {
+      /** Check if the current vehicle total population is greater than the current highest sum */
       if (vehicle["totalPopulation"] > highestSum) {
+        /** Update the highest sum to the current vehicle total population */
         highestSum = vehicle["totalPopulation"];
       }
     }
 
+    /** Go thorugh each vehicle from the parsed vehicles state */
     for (const vehicle of parsedVehicles) {
+      /** Check if the current vehicle total population is equal to the current highest sum */
       if (vehicle["totalPopulation"] === highestSum) {
+        /** Update the vehicles with the highest sum with the current vehicle */
         setVehiclesWithHighestSum(prevValue => [...prevValue, vehicle]);
       }
     }
@@ -114,20 +153,11 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Star Wars</h1>
       <Table vehiclesWithHighestSum={vehiclesWithHighestSum} />
       <BarChart />
     </div>
   );
 }
-
-// {
-//   planets: {
-//     keodf: 4000,
-//     rnfef: 2332...
-//   },
-//   pilots: {
-//     pilotName: planetName
-//   }
-// }
 
 export default App;
--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -10,28 +10,40 @@ const BarChart = () => {
     const [planets, setPlanets] = useState({})
 
     useEffect(() => {
+        /** Get the requested planets population to show in the bar chart */
         GetRequestedPlanets();
     }, [])
 
+    /**
+     * Get the requested planets data
+     */
     const GetRequestedPlanets = async () => {
-        var new_planets = {};
+        var planetsInBarChart = {};
 
+        /** Go thorugh each planet from the requested planets to show in the bar chart */
         for (const planet of BAR_PLANETS) {
+            /** Gets the current planet */
             var resPlanet = await axios.get(`${ROOT}planets/?search=${planet}`);
+
+            /** Update the current planet population */
             var population = resPlanet.data.results[0].population;
-            new_planets[planet] = population ;
+
+            /** Add the planet and its population value to the planets to show in the bar chart */
+            planetsInBarChart[planet] = population ;
         }
 
-        setPlanets(new_planets);
+        /** Update the planets state */
+        setPlanets(planetsInBarChart);
     }
 
     return (
         <div>
-            <table style={{width: "100%"}}>
+            <h4>Bar chart that compares the home planets’ own population</h4>
+            <table style={{ width: "100%", border: "1px solid black"}}>
                 <tbody>
                     <tr>
                         {Object.entries(planets).map(([planetName, population]) =>
-                            <td key={planetName}>
+                            <td key={planetName} style={{ verticalAlign: "bottom"}}>
                                 <Bar name={planetName} value={population}/>
                             </td>
                         )}
@@ -41,11 +53,6 @@ const BarChart = () => {
         </div>
     )
 };
-
-// planets = {
-//     "Tatooine": 2000,
-//         "Alderaan": 900
-// }
 
 export default BarChart;
 
